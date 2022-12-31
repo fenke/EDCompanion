d3eb3e1384b0282517794600332d8861fbfe9e87
fenke <fenke@live.com>
2022-12-31 13:50:28 +0100

    update

--- edcompanion/edsm_api.py
+++ edcompanion/edsm_api.py
@@ -22,6 +22,7 @@ def get_edsm_info(systemname, verbose=True):
     else:
         return {}
 
+    
 @lru_cache(512)
 def distance_between_systems(s1name,s2name):
     s1 = get_edsm_info(s1name)
@@ -29,3 +30,41 @@ def distance_between_systems(s1name,s2name):
     c1 = np.asarray([s1.get('coords',dict(x=0,y=0,z=0)).get(k) for k in ['x', 'y', 'z']])
     c2 = np.asarray([s2.get('coords',dict(x=0,y=0,z=0)).get(k) for k in ['x', 'y', 'z']])
     return np.sqrt(np.sum(np.square(c1-c2)))
+
+@lru_cache(32)
+def get_systems_in_cube(system, size=100):
+    base_url='https://www.edsm.net/api-v1/cube-systems'    
+    if not system:
+        return {}
+    req = requests.get(
+        base_url,
+        params=dict(
+            systemName=system, 
+            showCoordinates=1,
+            showPrimaryStar=1,
+            size=size
+        ) 
+    )
+    if req.status_code == 200:
+        return req.json()
+    else:
+        return {}
+    
+@lru_cache(32)
+def get_systems_in_sphere(system, radius=100):
+    base_url='https://www.edsm.net/api-v1/sphere-systems'    
+    if not system:
+        return {}
+    req = requests.get(
+        base_url,
+        params=dict(
+            systemName=system, 
+            showCoordinates=1,
+            showPrimaryStar=1,
+            radius=radius
+        )
+    )
+    if req.status_code == 200:
+        return req.json()
+    else:
+        return {}
